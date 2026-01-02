f1bc274c8c7c1aa18aab2f16db50b5c6050b458c
sulikowskim96 <sulikowskim96@gmail.com>
2026-01-02 20:03:38 +0000

    c

--- ios/Flutter/flutter_export_environment.sh
+++ ios/Flutter/flutter_export_environment.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 # This is a generated file; do not edit or check into version control.
-export "FLUTTER_ROOT=/home/ubuntu/flutter"
-export "FLUTTER_APPLICATION_PATH=/home/ubuntu/projects/hello_world"
+export "FLUTTER_ROOT=/home/user/flutter"
+export "FLUTTER_APPLICATION_PATH=/home/user/myapp"
 export "COCOAPODS_PARALLEL_CODE_SIGN=true"
 export "FLUTTER_TARGET=lib/main.dart"
 export "FLUTTER_BUILD_DIR=build"
